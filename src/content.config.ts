cc2f3388ed2eab377efd63a2599860ef5a00df4a
Luca Gonzalez <luca.gonzalezp@gmail.com>
2025-02-23 00:22:09 +0100

    Filtering draft posts from my RSS feed

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,19 @@
 import {glob} from 'astro/loaders';
-import {defineCollection} from 'astro:content';
-import {rssSchema} from "@astrojs/rss";
+import {defineCollection, z} from 'astro:content';
 
 const blog = defineCollection({
     // Load Markdown and MDX files in the `src/content/blog/` directory.
     loader: glob({base: './src/content/blog', pattern: '**/*.{md,mdx}'}),
     // Type-check frontmatter using a schema
-    schema: rssSchema,
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+        heroImage: z.string().optional(),
+        draft: z.boolean().optional(),
+    }),
 });
 
 export const collections = {blog};
